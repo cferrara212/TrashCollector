3affbb694c715342541cca3d9ff10b2244dd13a0
unknown <cferrara212@gmail.com>
2021-10-25 14:51:10 -0600

    added functionality for the employees/index.html and index views that gives the employee the day of the week and todays date

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,9 @@
+from django.db.models.fields import DateField
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.apps import apps
+from datetime import date
+import calendar
 
 # Create your views here.
 
@@ -10,4 +13,14 @@ from django.apps import apps
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    curr_date = date.today()
+    day_of_week = calendar.day_name[curr_date.weekday()]
+    todays_customers = Customer.objects.filter(weekly_pickup = day_of_week)
+    context = {
+        'curr_date': curr_date,
+        'day_of_week': day_of_week,
+        'todays_customers': todays_customers,
+    }
+    
+    return render(request, 'employees/index.html',context)
+    

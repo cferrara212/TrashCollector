38717403f876b9e8dbe545f25c660d428e427006
unknown <cferrara212@gmail.com>
2021-10-25 16:30:28 -0600

    remigrated MySQL library, updated Employees model and employees/views.py for creation of employee

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
-    employee_id = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=5)
+    route_zip= models.CharField(max_length=5, default=55555)
 
     def __str__(self):
         return self.name
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,26 +1,52 @@
 from django.db.models.fields import DateField
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from .models import Employee
 from datetime import date
 import calendar
+
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
+@login_required
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    curr_date = date.today()
-    day_of_week = calendar.day_name[curr_date.weekday()]
-    todays_customers = Customer.objects.filter(weekly_pickup = day_of_week)
-    context = {
-        'curr_date': curr_date,
-        'day_of_week': day_of_week,
-        'todays_customers': todays_customers,
-    }
-    
-    return render(request, 'employees/index.html',context)
+    logged_in_user = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+        Customer = apps.get_model('customers.Customer')
+        curr_date = date.today()
+        day_of_week = calendar.day_name[curr_date.weekday()]
+        todays_customers = Customer.objects.filter(weekly_pickup = day_of_week, )
+        context = {
+            'logged_in_employee':logged_in_employee,
+            'curr_date': curr_date,
+            'day_of_week': day_of_week,
+            'todays_customers': todays_customers,
+        }
+        
+        return render(request, 'employees/index.html',context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        route_from_form = request.POST.get('route_zip')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, route_zip=route_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        return render(request, 'customers/create.html')
     

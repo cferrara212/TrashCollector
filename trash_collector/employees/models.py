55ccbe3e3b4f7e54e5a8b07e95b9ab2ffb190827
unknown <cferrara212@gmail.com>
2021-10-25 13:48:43 -0600

    Fixed syntax in employees/models.py file. Imported Employee class model into apps.py and migrated new model into MySQL database

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Employee(models.model):
+class Employee(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
     employee_id = models.CharField(max_length=50)
